9b25ce90c33f690fc57e091ab0f1b361089e48ac
BassV <khorotyanvahagn@gmail.com>
2016-08-19 13:23:46 +0400

    version 0.00.011
    Added:
    
    1. The ability to Zoom in and out
      The user cannot zoom in or out too much
    2. The user can select an object and it becomes highlighted and can be visible through other objects
      whenever another object is selected, the previous selected one unselects, and the new one gets selected
    
    Fixed (changed):
    
    1. The camera is set to Perspective mode because the orthographic one caused some glitches
    2. The shadow quality is improved significantly (Before it was fuzzy)
      The problem was fixed by reducing shadow distance and letting the dual lightmapping handle distant shadows
    3. Whenever zooming in or out, some bugs occur because the code understands that the user wants to move the camera or rotate it
      Fix it by disabling the camera move and rotate functionality while zooming in and out

---
+++ Assets/Scripts/SelectNMoveObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SelectNMoveObject : MonoBehaviour {
+
+    public Material normalObjMaterial;
+    public Material selectedObjMaterial;
+
+    private List<GameObject> rayastedObjects = new List<GameObject>();
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        SelectObject();
+	}
+
+    void SelectObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 50f))
+            {
+                if (hit.transform.gameObject.tag == "StackerObject") // It is the blocks layer
+                {
+                    if (rayastedObjects.Count > 0)
+                    {
+                        rayastedObjects[0].transform.gameObject.GetComponent<Renderer>().material = normalObjMaterial;
+                        rayastedObjects.Remove(rayastedObjects[0]);
+                    }
+                    
+                    hit.transform.gameObject.GetComponent<Renderer>().material = selectedObjMaterial;
+                    rayastedObjects.Add(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ZoomConfig.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomConfig : MonoBehaviour
+{
+    public Camera mainCam;
+
+    private float perspectiveZoomSpeed = 0.3f;        // The rate of change of the field of view in perspective mode.
+    private float orthoZoomSpeed = 0.3f;        // The rate of change of the orthographic size in orthographic mode.
+
+    void Awake()
+    {
+        //mainCam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            MoveCam.canMoveTheCam = false;
+
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+            if (mainCam.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                mainCam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Make sure the orthographic size never drops below zero.
+                mainCam.orthographicSize = Mathf.Max(mainCam.orthographicSize, 1f);
+                mainCam.orthographicSize = Mathf.Min(mainCam.orthographicSize, 10f);
+            }
+            else
+            {
+                // Otherwise change the field of view based on the change in distance between the touches.
+                mainCam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                mainCam.fieldOfView = Mathf.Clamp(mainCam.fieldOfView, 15f, 120f);
+            }
+
+            MoveCam.canMoveTheCam = true;
+        }
+    }
+}
